3a00081d2d6e5ab07111a52dd7a0ff8195d69ed6
Axel Cordero <axeljcm@estudiantec.cr>
2023-06-01 18:12:05 -0600

    Codigo para pantalla lcd

---
+++ sketch_jun1a/codigo_pantalla/codigo_pantalla.ino
@@ -0,0 +1,28 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(0, 1, 2, 3, 12, 13);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Cantidad de cartas seleccionadas por el usuario
+  lcd.print("Cant Select:");
+  // Cantidad de cartas del Deck restantes
+  lcd.setCursor(0,1);
+  lcd.print("Restantes:");
+  
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(11, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+
+  lcd.setCursor(13, 0);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
