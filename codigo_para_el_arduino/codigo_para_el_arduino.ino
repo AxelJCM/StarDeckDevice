6e725ad3ccdf6929fb7990a25caa3881c8978dbf
Axel Cordero <axeljcm@estudiantec.cr>
2023-06-06 20:34:33 -0600

    Nuevo codigo pantalla solo funciona en protoboard

--- codigo_para_el_arduino/codigo_para_el_arduino.ino
+++ codigo_para_el_arduino/codigo_para_el_arduino.ino
@@ -23,8 +23,6 @@ int sel;
 
 
 void setup() {
-  lcd.begin(16, 2);
-  
   // Configurar el pin del motor como salida
   pinMode(motorSleft, OUTPUT);
   pinMode(motorGleft, OUTPUT);
@@ -38,68 +36,70 @@ void setup() {
 
   digitalWrite(select, LOW);
   digitalWrite(ready, LOW);  
-  Serial.begin(9600);
-  // Iniciar la pantalla LCD
-  
+  //Serial.begin(9600);
+
+  //Comenzar lcd
+  lcd.begin(16,2);
 }
 
 void loop() {
-  // Girar el motor en una dirección
-  //Serial.println(motorPin);
   on = digitalRead(encendido);
   sel = digitalRead(select);
-  delay(10000);
-  
+  delay(1000);
+  lcd.clear();
   if(on == 1){
-    // si el circuito esta encendido
-    // Cantidad de cartas seleccionadas por el usuario
-    //lcd.print("Cant Select:");
-    // Cantidad de cartas del Deck restantes
-    //lcd.setCursor(0,1);
-    //lcd.print("Restantes:");
-
-    //lcd.setCursor(11, 1);
-    //lcd.print(selCards);
-
-    //lcd.setCursor(13, 0);
-    //lcd.print(totalCards);
-
+    lcd.clear();
+    //Si el circuito esta encendido
+    //Cantidad de cartas seleccionadas por el usuario
+    lcd.print("Cant Select:");
+    //Cantidad de cartas del Deck restantes
+    lcd.setCursor(0,1);
+    lcd.print("Restantes:");
+    lcd.setCursor(11, 1);
+    lcd.print(totalCards);
+    lcd.setCursor(13, 0);
+    lcd.print(selCards);
+    
     if(totalCards == 0){
       digitalWrite(ready, 255);
       // Pantalla LCD Finalizada
-      //lcd.print("Seleccion");
-      //lcd.setCursor(0,1);
-      //lcd.print("Finalizada :)");
-    }else if(sel == 1 ){// si el usuario selecciona una carta
+      lcd.clear();
+      lcd.print("Seleccion");
+      lcd.setCursor(0,1);
+      lcd.print("Finalizada :)");
+      
+    // si el usuario selecciona una carta
+    }else if(sel == 1){
       if(selCards <7){
+      // encender motores izq por 500ms
+      analogWrite(motorSleft, 255); 
+      // Establecer la velocidad máxima (sentido de giro depende de la conexión)
+      delay(100);
+      analogWrite(motorGleft, 255);
+      delay(1000); // Esperar 1 segundos
+      // Detener el motor
+      analogWrite(motorGleft, 0);
+      analogWrite(motorSleft, 0);
+      // aumentar contadores
+      selCards += 1;
+      totalCards -= 1;
+      
+      }else{
         // encender motores izq por 500ms
-        analogWrite(motorSleft, 255); 
-        // Establecer la velocidad máxima (sentido de giro depende de la conexión)
-        delay(100);
-        analogWrite(motorGleft, 255);
+        analogWrite(motorSright, 255); // Establecer la velocidad máxima (sentido de giro depende de la conexión)
+        delay(1000);
+        analogWrite(motorGright, 255);
         delay(1000); // Esperar 1 segundos
         // Detener el motor
-        analogWrite(motorGleft, 0);
-        analogWrite(motorSleft, 0);
+        analogWrite(motorGright, 0);
+        analogWrite(motorSright, 0);
         // aumentar contadores
-        selCards += 1;
         totalCards -= 1;
-    }else{
-      lcd.print("Error: Ya no se");
-      lcd.setCursor(0,1);
-      lcd.print("puede seleccionar");
-      // encender motores izq por 500ms
-      analogWrite(motorSright, 255); // Establecer la velocidad máxima (sentido de giro depende de la conexión)
-      delay(100);
-      analogWrite(motorGright, 255);
-      delay(1000); // Esperar 1 segundos
-      // Detener el motor
-      analogWrite(motorGright, 0);
-      analogWrite(motorSright, 0);
-      // aumentar contadores
-      totalCards -= 1;
-    }
-      
+        lcd.clear();
+        lcd.print("Error: Ya no se");
+        lcd.setCursor(0,1);
+        lcd.print("puede seleccionar");
+        }
     }else{
       // encender motores izq por 500ms
       analogWrite(motorSright, 255); // Establecer la velocidad máxima (sentido de giro depende de la conexión)
@@ -111,10 +111,10 @@ void loop() {
       analogWrite(motorSright, 0);
       // aumentar contadores
       totalCards -= 1;
-    }
+      }
   }else{
+    lcd.clear();
     lcd.print("Encender Switch");
     lcd.setCursor(0,1);
     lcd.print("Para Comenzar :)");
-  }
-}
+  }}
